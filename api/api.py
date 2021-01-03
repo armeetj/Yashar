d8ea7491f102717b4ead2af7ef806effde96530f
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-02 16:45:06 -0800

    changed demo.html to index.html

--- api/api.py
+++ api/api.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+import tensorflow as tf
+from tensorflow import keras
+import pickle
 
 app = Flask(__name__)
 api = Api(app)
 
-# model = tf.load()
+# load AI model (pretrained)
+# model = keras.models.load_model('model/model.h5')
+
+file = open('model/model.h5', 'r')
+file_data = file.read()
+
+model = pickle.loads(file_data)
+print(model.summary)
 
 class Emotions(Resource):
     def get(self):
@@ -19,7 +29,7 @@ class Emotions(Resource):
 
         if (str.__contains__(file.filename, ".png") == False) and (str.__contains__(file.filename, ".jpg") == False):
             return {"error": "Files must be of format png or jpg!"}
-        return {"result": [{"happy": 0.1}, {"angry": 0.2}]}
+        return {"angry": 0.1, "disgusted": 0, "fearful": 0, "happy": 0.4, "sad": 0.2, "surprised": 0.3, "neutral": 0}
 
 
 api.add_resource(Emotions, '/v1/emotions')
